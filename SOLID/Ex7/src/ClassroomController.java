c8b307fa6be475edcbc3bf3ce36351c4674f7418
Khushi Sarawagi <khushikhush006@gmail.com>
2026-03-02 11:25:33 +0530

    ex7 sol

--- SOLID/Ex7/src/ClassroomController.java
+++ SOLID/Ex7/src/ClassroomController.java
@@ -4,24 +4,24 @@ public class ClassroomController {
     public ClassroomController(DeviceRegistry reg) { this.reg = reg; }
 
     public void startClass() {
-        SmartClassroomDevice pj = reg.getFirstOfType("Projector");
+        Projector pj = (Projector) reg.getFirstPowerableOfType("Projector");
         pj.powerOn();
         pj.connectInput("HDMI-1");
 
-        SmartClassroomDevice lights = reg.getFirstOfType("LightsPanel");
+        BrightnessControl lights = reg.getFirstOfType(BrightnessControl.class);
         lights.setBrightness(60);
 
-        SmartClassroomDevice ac = reg.getFirstOfType("AirConditioner");
+        TemperatureControl ac = reg.getFirstOfType(TemperatureControl.class);
         ac.setTemperatureC(24);
 
-        SmartClassroomDevice scan = reg.getFirstOfType("AttendanceScanner");
+        AttendanceCapable scan = reg.getFirstOfType(AttendanceCapable.class);
         System.out.println("Attendance scanned: present=" + scan.scanAttendance());
     }
 
     public void endClass() {
         System.out.println("Shutdown sequence:");
-        reg.getFirstOfType("Projector").powerOff();
-        reg.getFirstOfType("LightsPanel").powerOff();
-        reg.getFirstOfType("AirConditioner").powerOff();
+        reg.getFirstPowerableOfType("Projector").powerOff();
+        reg.getFirstPowerableOfType("LightsPanel").powerOff();
+        reg.getFirstPowerableOfType("AirConditioner").powerOff();
     }
 }
--- SOLID/Ex7/src/DeviceRegistry.java
+++ SOLID/Ex7/src/DeviceRegistry.java
@@ -1,13 +1,23 @@
 import java.util.*;
 
 public class DeviceRegistry {
-    private final java.util.List<SmartClassroomDevice> devices = new ArrayList<>();
+    private final java.util.List<Object> devices = new ArrayList<>();
 
-    public void add(SmartClassroomDevice d) { devices.add(d); }
+    public void add(Object d) { devices.add(d); }
 
-    public SmartClassroomDevice getFirstOfType(String simpleName) {
-        for (SmartClassroomDevice d : devices) {
-            if (d.getClass().getSimpleName().equals(simpleName)) return d;
+    @SuppressWarnings("unchecked")
+    public <T> T getFirstOfType(Class<T> capability) {
+        for (Object d : devices) {
+            if (capability.isInstance(d)) return (T) d;
+        }
+        throw new IllegalStateException("Missing device with capability: " + capability.getSimpleName());
+    }
+
+    public Powerable getFirstPowerableOfType(String simpleName) {
+        for (Object d : devices) {
+            if (d.getClass().getSimpleName().equals(simpleName) && d instanceof Powerable) {
+                return (Powerable) d;
+            }
         }
         throw new IllegalStateException("Missing: " + simpleName);
     }
